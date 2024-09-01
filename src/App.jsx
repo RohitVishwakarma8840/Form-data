2926c3e9ee996cd4eae1b3f6a7aabcd5ec17d112
Rohit <rohit8840185@gmail.com>
2024-09-01 14:31:57 +0530

    adding code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,49 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  const model={
+   firstname:'',
+   lastname:'',
+   email:'',
+   password:'',
+   username:'',
+   mobile:''
+ 
+  }
+
+  const getFormValue=(e)=>{
+    
+    const input= e.target
+    // console.log(input.value);
+    const value= input.value;
+    const key= input.name;
+    setForm({
+      ...form,
+    [key]: value
+
+    })  
+
+  }
+
+  const signUp = (e) =>{
+     e.preventDefault()
+     console.log(form);  
+
+  }
+
+  const [form, setForm] = useState(model);
 
   return (
     <>
-      <div className='bg-gray-400 h-screen flex justify-center items-center '>
+      <div className='bg-gray-100 h-screen flex justify-center items-center '>
+        {/* <h1>{JSON.stringify(form)}</h1> */}
         <div className='bg-white px-8 py-6 w-[350px] shadow-lg rounded-lg '>
-          <form >
+          <h1 className='text-2xl font-bold  px-3 mb-4 text-center'>Rohit Sign Up Form</h1>
+          <form onSubmit={signUp}>
+
        <div className='flex flex-col gap-2'>
  
        <label className='text-lg font-bold' >Firstname</label>
@@ -19,6 +52,7 @@ function App() {
        type="text"
        placeholder="enter your first name"
        className="border-gray-400 p-2"
+      onChange={getFormValue}
         />
     {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -30,10 +64,12 @@ function App() {
  
  <label className='text-lg font-bold' >Lastname</label>
  <input 
- name="Lastname"
+ name="lastname"
  type="text"
  placeholder="enter your Last name"
  className="border-gray-400 p-2"
+ onChange={getFormValue}
+
   />
 {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -47,6 +83,8 @@ function App() {
  type="email"
  placeholder="enter your email here "
  className="border-gray-400 p-2"
+   onChange={getFormValue}
+
   />
 {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -60,6 +98,8 @@ function App() {
  type="password"
  placeholder=" ********"
  className="border-gray-400 p-2"
+      onChange={getFormValue}
+
   />
 {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -74,6 +114,8 @@ function App() {
  type="text"
  placeholder="enter username here "
  className="border-gray-400 p-2"
+     onChange={getFormValue}
+
   />
 {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -83,10 +125,12 @@ function App() {
  
  <label className='text-lg font-bold' >Mobile No</label>
  <input 
- name="username"
+ name="mobile"
  type="number"
  placeholder="8976542236 "
  className="border-gray-400 p-2"
+     onChange={getFormValue}
+
   />
 {/* <small className='text-rose-600 text-semibold text-sm'>This field is required </small> */}
 
@@ -94,7 +138,7 @@ function App() {
   
   <br />
 
- <button className=' border-0 bg-indigo-600 w-full gap-4 px-0 text-white'>Submit</button>
+ <button className=' border-0 bg-indigo-600 w-full rounded gap-4 px-0 text-white'>Submit</button>
 
           </form>
 
